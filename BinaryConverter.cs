e6bbf85dce9bb4cb422843c94363d731044de115
mobolaji <bjwealthy@gmail.com>
2023-09-24 15:12:53 +0100

    first commit

---
+++ BinaryConverter.cs
@@ -0,0 +1,12 @@
+public class BinaryConverter{
+    public string StringToBinary(string str){
+        string binaryString = "";
+        
+        foreach (char c in str){
+            string binary = Convert.ToString(c, 2).PadLeft(8, '0');
+            binaryString += binary + " ";
+        }
+
+        return binaryString.Trim();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+class Program{       
+    static void Main(string[] args){
+            //step 1:
+        Console.WriteLine("What is your name?");
+        
+        var name = Console.ReadLine();
+
+        BinaryConverter bc = new BinaryConverter();
+
+        string nameInBin = bc.StringToBinary(name);
+
+        Console.WriteLine(nameInBin);
+    }
+    
+        
+}
